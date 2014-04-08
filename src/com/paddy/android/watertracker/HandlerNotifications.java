6b9a7337725e6e31d5c2ff96e53b4174b2c0f8ec
Matt Spitz <mattspitz@gmail.com>
2014-04-08 01:14:01 -0700

    Greatly simplify notifications code by using IntentService and moving notifications logic into HandlerNotifications.

--- src/com/paddy/android/watertracker/HandlerNotifications.java
+++ src/com/paddy/android/watertracker/HandlerNotifications.java
@@ -1,21 +1,22 @@
 package com.paddy.android.watertracker;
 
 import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.os.AsyncTask;
-import android.os.IBinder;
-import android.os.PowerManager;
-import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
-public class HandlerNotifications extends Service {
+public class HandlerNotifications extends IntentService {
 
 	public static final String TAG = "NH";
-	private WakeLock mWakeLock;
+
+	public HandlerNotifications(String name) {
+		super(name);
+	}
 
 	public static void configureRepeatingAlarm(Context context, long interval) {
 		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -27,49 +28,30 @@ public class HandlerNotifications extends Service {
 				interval, interval, pi);
 	}
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return null;
+	public void onHandleIntent(Intent intent) {
+		notifyUser();
 	}
 
-	@SuppressWarnings("deprecation")
-	public void handleIntent(Intent intent) {
-		PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-		mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
-		mWakeLock.acquire();
+	private void notifyUser() {
+		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
 
-		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-		if (!cm.getBackgroundDataSetting()) {
-			stopSelf();
-			return;
-		}
-		new NotificationTask().execute();
+		notificationBuilder.setSmallIcon(R.drawable.water);
+		notificationBuilder.setContentTitle("Water Tracker");
+		notificationBuilder.setContentText("Time to drink!");
+		notificationBuilder.setAutoCancel(true);
+
+		Intent intent = new Intent(this, DrunkTodayActivity.class);
+
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		stackBuilder.addParentStack(DrunkTodayActivity.class);
+		stackBuilder.addNextIntent(intent);
+		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		resultPendingIntent.cancel();
+
+		notificationBuilder.setContentIntent(resultPendingIntent);
+
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(0, notificationBuilder.build());
 	}
-
-	private class NotificationTask extends AsyncTask<Void, Void, Void> {
-
-		@Override
-		protected Void doInBackground(Void... params) {
-			Log.i("doInBackground", "called");
-			startService(new Intent(HandlerNotifications.this, CreatorNotifications.class));
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			stopSelf();
-		}
-	}
-
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		handleIntent(intent);
-		return START_NOT_STICKY;
-	}
-
-	public void onDestroy() {
-		super.onDestroy();
-		mWakeLock.release();
-	}
-
-
 }
