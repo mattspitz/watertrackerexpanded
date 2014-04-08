2ce653d1adaf0ab0015fa9fc29e78b26331797f8
Matt Spitz <mattspitz@gmail.com>
2014-04-08 01:06:11 -0700

    Our boot receiver should just configure the interval as well.

--- src/com/paddy/android/watertracker/ReceiveBoot.java
+++ src/com/paddy/android/watertracker/ReceiveBoot.java
@@ -1,25 +1,14 @@
 package com.paddy.android.watertracker;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.SystemClock;
 
-public class ReceiveBoot {
+public class ReceiveBoot extends BroadcastReceiver {
 	
+	@Override
 	public void onReceive(Context context, Intent intent) {
-		
-		int hours = 1;
-		AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		Intent i = new Intent(context, HandlerNotifications.class);
-		PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
-		am.cancel(pi);
-		
-		am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, 
-				SystemClock.elapsedRealtime() + hours*60*30*1000, 
-				hours*60*30*1000, pi);
-			
-		
+		long temporaryInterval = 30000; // FIXME every 30 seconds
+		HandlerNotifications.configureRepeatingAlarm(context, temporaryInterval);
 	}
 }
